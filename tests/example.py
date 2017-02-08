ff0f72ec8e47727e6dc8e8488fe99cf01f36754b
George Y. Kussumoto <georgeyk.dev@gmail.com>
2017-02-08 21:21:31 -0200

    Update examples

--- tests/example.py
+++ tests/example.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# vi:si:et:sw=4:sts=4:ts=4
-
 import logging
 
 from belogging.loader import BeloggingLoader
@@ -11,16 +8,15 @@ loader.setup()
 
 
 logger = logging.getLogger('foo')
-logger.debug('debug')
-logger.info('info')
-logger.error('error')
-logger.warning('warning')
-logger.critical('critical')
+logger.debug('foo debug')
+logger.info('foo info')
 
 
 logger2 = logging.getLogger('foo.bar')
-logger2.debug('debug')
-logger2.info('info')
-logger2.error('error')
-logger2.warning('warning')
-logger2.critical('critical')
+logger2.debug('foo.bar debug')
+logger2.info('foo.bar info')
+
+
+logger3 = logging.getLogger('bar')
+logger2.debug('bar debug')
+logger2.error('bar error')
---
+++ tests/json_example.py
@@ -0,0 +1,11 @@
+import logging
+
+from belogging.loader import BeloggingLoader
+
+
+loader = BeloggingLoader(json=True)
+loader.setup()
+
+
+logger = logging.getLogger('foo')
+logger.info('foo')
