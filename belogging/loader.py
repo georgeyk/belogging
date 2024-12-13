61483bef1d28348f7dc14a5a923da6f8aa988570
George Y. Kussumoto <georgeyk.dev@gmail.com>
2024-12-13 14:11:21 -0300

    Fix: upgrade python-json-formatter version

--- belogging/loader.py
+++ belogging/loader.py
@@ -2,14 +2,18 @@ import logging
 import logging.config
 from copy import deepcopy
 
-from pythonjsonlogger.jsonlogger import JsonFormatter
+from pythonjsonlogger.json import JsonFormatter
 
-from .defaults import DEFAULT_JSON_FORMAT, DEFAULT_KVP_FORMAT, DEFAULT_LOGGING_CONF, LEVEL_MAP
+from .defaults import (
+    DEFAULT_JSON_FORMAT,
+    DEFAULT_KVP_FORMAT,
+    DEFAULT_LOGGING_CONF,
+    LEVEL_MAP,
+)
 from .exceptions import ConfigurationWarning
 
 
 class BeloggingLoader:
-
     def __init__(self, capture_warnings=True, use_default_kvp=True, json=False):
         self._config = deepcopy(DEFAULT_LOGGING_CONF)
 
@@ -20,7 +24,7 @@ class BeloggingLoader:
             self.enable_json_formatter()
 
         # Custom level to suppress handlers
-        logging.addLevelName('DISABLED', LEVEL_MAP['DISABLED'])
+        logging.addLevelName("DISABLED", LEVEL_MAP["DISABLED"])
         logging.captureWarnings(capture_warnings)
 
     @property
@@ -28,22 +32,24 @@ class BeloggingLoader:
         return self._config
 
     def enable_json_formatter(self):
-        formatter = '{}.{}'.format(JsonFormatter.__module__, JsonFormatter.__name__)
-        self.config['formatters']['json'] = {'()': formatter,
-                                             'format': DEFAULT_JSON_FORMAT}
-        self.config['handlers']['default']['formatter'] = 'json'
+        formatter = "{}.{}".format(JsonFormatter.__module__, JsonFormatter.__name__)
+        self.config["formatters"]["json"] = {
+            "()": formatter,
+            "format": DEFAULT_JSON_FORMAT,
+        }
+        self.config["handlers"]["default"]["formatter"] = "json"
 
     def update_default_formatter(self, format_string):
-        self.config['formatters']['default']['format'] = format_string
+        self.config["formatters"]["default"]["format"] = format_string
 
     def add_filter(self, filter_name, filter_path):
-        ft = {filter_name: {'()': filter_path}}
-        if filter_name in self.config['filters']:
+        ft = {filter_name: {"()": filter_path}}
+        if filter_name in self.config["filters"]:
             msg = '"{}" already configured and not overwritten'.format(filter_name)
             raise ConfigurationWarning(msg)
 
-        self.config['filters'].update(ft)
-        self.config['handlers']['default']['filters'].append(filter_name)
+        self.config["filters"].update(ft)
+        self.config["handlers"]["default"]["filters"].append(filter_name)
 
     def setup(self):
         logging.config.dictConfig(self.config)
--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ from setuptools import Command, find_packages, setup
 here = os.path.abspath(os.path.dirname(__file__))
 version = "0.0.0"
 changes = os.path.join(here, "CHANGES.rst")
-pattern = r'^(?P<version>[0-9]+.[0-9]+(.[0-9]+)?)'
-with codecs.open(changes, encoding='utf-8') as changes:
+pattern = r"^(?P<version>[0-9]+.[0-9]+(.[0-9]+)?)"
+with codecs.open(changes, encoding="utf-8") as changes:
     for line in changes:
         match = re.match(pattern, line)
         if match:
@@ -19,7 +19,7 @@ with codecs.open(changes, encoding='utf-8') as changes:
 
 
 class VersionCommand(Command):
-    description = 'Show library version'
+    description = "Show library version"
     user_options = []
 
     def initialize_options(self):
@@ -32,50 +32,50 @@ class VersionCommand(Command):
         print(version)
 
 
-with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = '\n{}'.format(f.read())
+with codecs.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = "\n{}".format(f.read())
 
-with codecs.open(os.path.join(here, 'CHANGES.rst'), encoding='utf-8') as f:
+with codecs.open(os.path.join(here, "CHANGES.rst"), encoding="utf-8") as f:
     changes = f.read()
-    long_description += '\n\nChangelog:\n----------\n\n{}'.format(changes)
+    long_description += "\n\nChangelog:\n----------\n\n{}".format(changes)
 
 
 # Requirements
 
 # Unduplicated tests_requirements and requirements/test.txt
-tests_requirements = ['pytest', 'pytest-cov', 'coveralls', 'tox']
-install_requirements = ['python-json-logger>=0.1.5']
+tests_requirements = ["pytest", "pytest-cov", "coveralls", "tox"]
+install_requirements = ["python-json-logger>=3.2.9"]
 
 
 # setup
 
 setup(
-    name='belogging',
+    name="belogging",
     version=version,
-    description='Belogging',
+    description="Belogging",
     long_description=long_description,
-    url='https://github.com/georgeyk/belogging/',
-    download_url='https://github.com/georgeyk/belogging/releases',
-    license='MIT',
-    author='George Y. Kussumoto',
-    author_email='contato@georgeyk.com.br',
-    packages=find_packages(exclude=['docs', 'tests', 'tests.*', 'requirements*']),
+    url="https://github.com/georgeyk/belogging/",
+    download_url="https://github.com/georgeyk/belogging/releases",
+    license="MIT",
+    author="George Y. Kussumoto",
+    author_email="contato@georgeyk.com.br",
+    packages=find_packages(exclude=["docs", "tests", "tests.*", "requirements*"]),
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: System :: Logging',
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: System :: Logging",
     ],
-    keywords='logging',
-    setup_requires=['pytest-runner'],
+    keywords="logging",
+    setup_requires=["pytest-runner"],
     install_requires=install_requirements,
     tests_require=tests_requirements,
-    cmdclass={'version': VersionCommand},
+    cmdclass={"version": VersionCommand},
 )
